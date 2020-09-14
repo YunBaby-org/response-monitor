af7196e6e537ad286edca35becdd45fdb5d8c198
garyparrot <weedbug666@gmail.com>
2020-09-14 20:29:47 +0800

    feat: evaluate tracker_id when insert request

--- src/query/tracker-requests.sql
+++ src/query/tracker-requests.sql
@@ -1,15 +1,15 @@
 /* 
   @name insertRequest
-  @param requests -> ((id, time, request)...)
+  @param requests -> ((tracker_id, id, time, request)...)
  */
-INSERT INTO public.tracker_requests(request_id, delivery_time, request)
+INSERT INTO public.tracker_requests(trackerid, request_id, delivery_time, request)
   VALUES :requests;
 
 /* 
   @name insertResponse
-  @param responses -> ((id, time, response)...)
+  @param responses -> ((tracker_id, id, time, response)...)
  */
-INSERT INTO public.tracker_requests(request_id, received_time, response)
+INSERT INTO public.tracker_requests(trackerid, request_id, received_time, response)
   VALUES :responses
   ON CONFLICT (request_id) DO UPDATE SET
     received_time = excluded.received_time,
